917af9ff00d9836eb70651fca785b0f958822c3a
shivvang <shivanghariakar@email.com>
2024-07-03 10:07:42 +0530

    added copy current page link feature and some other frontend details of property view page

--- client/src/pages/PropertyView.jsx
+++ client/src/pages/PropertyView.jsx
@@ -3,6 +3,16 @@ import { Form, useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Swipercore from "swiper";
 import { Navigation } from "swiper/modules";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+
 import "swiper/css/bundle";
 
 function PropertyView() {
@@ -10,6 +20,7 @@ function PropertyView() {
   const [propertyData, setPropertydata] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
   useEffect(() => {
     const fetchData = async () => {
@@ -57,6 +68,68 @@ function PropertyView() {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            {/* the Clipboard API to copy the current page URL
+            (window.location.href) to the clipboard.
+            navigator.clipboard.writeText is a promise-based method that writes
+            the provided text to the system clipboard. */}
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {propertyData.name} - ${" "}
+              {propertyData.priceBreakUp.toLocaleString("en-US")}
+              {propertyData.propertyType === "rent" && " / month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {propertyData.address}
+            </p>
+            <div>
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {propertyData.propertyType === "rent" ? "for rent" : "for sale"}
+              </p>
+            </div>
+            <p className="text-slate-800">
+              <strong>Description -</strong> {propertyData.description}
+            </p>
+            <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaBed className="text-lg" />
+                {propertyData.beds > 1
+                  ? `${propertyData.beds} beds`
+                  : `${propertyData.beds} bed`}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaBath className="text-lg" />
+                {propertyData.baths > 1
+                  ? `${propertyData.baths} beds`
+                  : `${propertyData.baths} bed`}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaParking className="text-lg" />
+                {propertyData.parking ? "parking" : "No parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaChair className="text-lg" />
+                {propertyData.furnished ? "furnished" : "Unfurnished"}
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </main>
