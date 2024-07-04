11efffb2607ba9083f590edffbf48f7bf76ddd97
shivvang <shivanghariakar@email.com>
2024-07-04 11:05:39 +0530

    completed navbar code updation , for search functionality

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,27 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import React from "react";
+import React, { useEffect, useState } from "react";
 function NavBar() {
+  const [serarchTerm, setSearchTerm] = useState("");
   const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("serarchTerm", serarchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
+  //used to keep the search term in sync with the URL query parameter.
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-gray-900 text-white">
       <div className="container mx-auto flex justify-between items-center py-4">
@@ -12,17 +29,19 @@ function NavBar() {
           Housing<span className="text-gray-400">.Com</span>
         </Link>
 
-        <form className="flex items-center">
+        <form onSubmit={handleFormSubmission} className="flex items-center">
           <input
             type="text"
             placeholder="Search"
             className="px-4 py-2 rounded-l-lg focus:outline-none focus:ring focus:border-blue-300 bg-gray-800 text-white"
+            value={serarchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button className="bg-blue-600 px-4 py-2 rounded-r-lg flex items-center">
             <FaSearch className="text-white text-lg" />
           </button>
         </form>
-        <ul className="flex gap-6">
+        <ul className="hidden md:flex items-center gap-4 lg:gap-6">
           <li className="text-sm">
             <Link to="/" className="hover:text-blue-500">
               Home
